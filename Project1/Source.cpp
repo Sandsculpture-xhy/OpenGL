3b437604e133d83994ec4ae59d9207a07ba1a228
Sandsculpture-xhy <iam_harry@163.com>
2021-12-12 16:07:33 -0600

    qwq

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <GL/gl.h>
-#include <glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <time.h>
 const char *PRONAME = "Project1";
+const char *LOGNAME = "Project1.log";
+struct tm *logtime;
+void REFRESHLOGTIME()
+{
+    time_t timer;
+    time(&timer);
+    logtime = localtime(&timer);
+}
 int main()
 {
+    freopen(LOGNAME, "w", stdout);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, PRONAME, NULL, NULL);
     if (window == NULL)
     {
-        // std::cout << "Failed to create GLFW window" << std::endl;
+        REFRESHLOGTIME();
+        printf("[%s](%d:%d:%d): Failed to create GLFW window\n", PRONAME, logtime->tm_hour, logtime->tm_min, logtime->tm_sec);
         glfwTerminate();
         return -1;
     }
+    REFRESHLOGTIME();
+    printf("[%s](%d:%d:%d): Create GLFW window success\n", PRONAME, logtime->tm_hour, logtime->tm_min, logtime->tm_sec);
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        // std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
     return 0;
 }
