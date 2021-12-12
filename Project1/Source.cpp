6d613059aaa4b6ea47d5691a7e3459c76e480e65
Sandsculpture-xhy <iam_harry@163.com>
2021-12-11 23:26:19 -0600

    qwq

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <GL/gl.h>
 #include <glfw3.h>
-const char* PRONAME = "Project1";
+const char *PRONAME = "Project1";
 int main()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        // std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
     return 0;
 }
