03e12782041bf50b6a41bb08c4a4f6dc11e42b44
Sandsculpture-xhy <iam_harry@163.com>
2021-12-11 23:23:12 -0600

    qwq

---
+++ Project1/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <GL/gl.h>
+#include <glfw3.h>
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    return 0;
+}
